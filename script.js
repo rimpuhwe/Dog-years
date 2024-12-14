631824fc1269cb5ca06b83724459caa024fb806b
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-14 15:06:13 +0200

    practicing the human years in dog years

---
+++ script.js
@@ -0,0 +1,9 @@
+const myAge = 10;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Raissa' .toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
